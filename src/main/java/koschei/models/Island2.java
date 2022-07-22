8046a9d5af020e57857d0b02f491423b9aa05436
Antony Voloshin <49843612+tohamdhope@users.noreply.github.com>
2022-07-22 07:10:01 +0300

    Done

--- src/main/java/koschei/models/Island2.java
+++ src/main/java/koschei/models/Island2.java
@@ -1,8 +1,9 @@
 package koschei.models;
+import org.springframework.stereotype.Component;
 
 public class Island2 {
 
-    private Wood3 wood;
+    private final Wood3 wood;
 
     public Island2(Wood3 wood) {
         this.wood = wood;
--- src/main/java/koschei/models/Ocean1.java
+++ src/main/java/koschei/models/Ocean1.java
@@ -1,13 +1,15 @@
 package koschei.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Ocean1 {
 
     @Autowired
-    private Island2 island;
+    private  Island2 island;
+
 
     @Override
     public String toString() {
